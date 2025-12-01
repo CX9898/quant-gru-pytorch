5dfc527b1dd48a2d3dfc89722cc2e222ffd5a529
Chengxing Zou <zcx5613@gmail.com>
2025-12-01 10:52:11 +0800

    update gru_interface

--- include/gru_interface.hpp
+++ include/gru_interface.hpp
@@ -1,20 +1,10 @@
 #pragma once
 
-#include <vector>
 #include <cublas_v2.h>
+#include <vector>
 
 #include "gru.h"
 #include "gru_quant.h"
-
-GRUQuantitativeParameters calibrateGruScales(
-    bool use_int16,
-    int time_steps, int batch_size, int input_size, int hidden_size,
-    const float *W,
-    const float *R,
-    const float *bx,
-    const float *br,
-    const float *x,
-    const cublasHandle_t &g_blas_handle);
 
 void calibrateGruScales(
     bool use_int16,
@@ -26,3 +16,49 @@ void calibrateGruScales(
     const std::vector<float> &x,
     const cublasHandle_t &g_blas_handle,
     GRUQuantitativeParameters &quant_gru_scales);
+
+GRUQuantitativeParameters calibrateGruScales(
+    bool use_int16,
+    int time_steps, int batch_size, int input_size, int hidden_size,
+    const float *W,
+    const float *R,
+    const float *bx,
+    const float *br,
+    const float *x,
+    const cublasHandle_t &g_blas_handle);
+
+template<typename QuantT>
+void quantitativeWeight(const int input_size, const int hidden_size,
+                        const float *W, const float *R, const float *bx, const float *br,
+                        const GRUQuantitativeParameters &quant_parms,
+                        QuantT *W_quant, QuantT *R_quant, int32_t *bx_quant, int32_t *br_quant);
+
+template<typename QuantT>
+void quantGRUForward(const int time_steps, const int batch_size, const int input_size,
+                     const int hidden_size, const QuantT *W, const QuantT *R, const int32_t *bx,
+                     const int32_t *br, const float *x,
+                     const GRUQuantitativeParameters &quant_parms,
+                     const cublasHandle_t &g_blas_handle,
+                     float *h// (time_steps) * batch_size * hidden_size
+);
+
+void hasteGRUForward(const int time_steps,
+                     const int batch_size,
+                     const int input_size,
+                     const int hidden_size,
+                     const float *W, const float *R, const float *bx,
+                     const float *br, const float *x,
+                     const cublasHandle_t &g_blas_handle,
+                     float *h);
+
+void forwardInterface(bool is_quant,
+                      bool use_int16,
+                      int time_steps, int batch_size, int input_size, int hidden_size,
+                      const float *W,
+                      const float *R,
+                      const float *bx,
+                      const float *br,
+                      const float *x,
+                      const GRUQuantitativeParameters &quant_gru_scales,
+                      const cublasHandle_t &g_blas_handle,
+                      float *h);
